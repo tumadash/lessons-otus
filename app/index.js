017f166d29bdd2ffdc2044b6425dd43711ed6f4e
tumadash <dasha.tumanova@inbox.ru>
2019-09-30 22:16:12 +0300

    javascript-1. sum(10)()()()(1)(1)(1)()(). Не работает "==="

--- app/index.js
+++ app/index.js
@@ -1,22 +1,21 @@
 function sum(num) {
 
     let currentSum = 0;
-    if (!num) {
-        return currentSum;
-    }
-    if (typeof num === 'number') {
+    if (num === undefined) {
+        currentSum = 0;
+    } else if (typeof num === 'number') {
         currentSum = num;
     } else {
         console.error('enter number');
         return;
     }
 
-    function func(b) {
-        if (!b) {
-            return currentSum;
+    function func(a) {
+        if (a === undefined) {
+            a = 0;
         }
-        if (typeof b === 'number') {
-            currentSum += b;
+        if (typeof a === 'number') {
+            currentSum += a;
         } else {
             console.error('enter number');
             return;
