51f01ec1aa6204d4bb9031f69a5799971afc9028
tumadash <dasha.tumanova@inbox.ru>
2020-01-31 15:08:27 +0300

    Homework. Создание одностраничного приложения. Camera

--- guestProject/src/containers/ImagePicker.js
+++ guestProject/src/containers/ImagePicker.js
@@ -2,8 +2,7 @@ import React, {useState, useCallback} from 'react';
 import {Image, View, TouchableOpacity, Text} from 'react-native';
 import ImagePickerLib from 'react-native-image-picker';
 import ImageResizer from 'react-native-image-resizer';
-
-// More info on all the options is in the API Reference... just some common use cases shown here
+import {Button, Icon, Avatar} from 'react-native-elements';
 const options = {
     title: 'Select Image',
     customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
@@ -28,10 +27,6 @@ export const ImagePicker = () => {
                 console.log(response);
                 const source = {uri: response.uri};
                 setImage(source);
-                // You can also display the image using data:
-                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-                // you can compress image
                 ImageResizer.createResizedImage(
                     response.uri,
                     response.width / 6,
@@ -53,12 +48,8 @@ export const ImagePicker = () => {
     }, []);
     return (
         <View style={{alignItems: 'center'}}>
-            <TouchableOpacity
-                onPress={pickImage}
-                style={{backgroundColor: 'green', padding: 24}}>
-                <Text>Select Image</Text>
-            </TouchableOpacity>
-            {image && <Image source={image} style={{width: 300, height: 300}} />}
+            {image ? <Avatar size={"large"} rounded source={image} onPress={pickImage}/> :
+                <Avatar size={"large"} rounded icon={{name: 'user', type: 'font-awesome'}} onPress={pickImage}/>}
         </View>
     );
 };
